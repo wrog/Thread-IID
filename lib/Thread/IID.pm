9626c6d1f3c95c1b204d4617090ddafabc2a7914
Roger Crew <crew@cs.stanford.edu>
2011-12-02 11:50:11 -0800

    doc cleanups

--- lib/Thread/IID.pm
+++ lib/Thread/IID.pm
@@ -48,12 +48,12 @@ Thread::IID - unique perl Interpreter IDs
 
 This provides an identifier to distinguish Perl interpreter instances.
 
-In environments like L<mod_perl2|mod_perl2>, where interpreters are
+In environments like L<mod_perl2|mod_perl2>, where interpreters can be
 cloned and passed around between OS threads, the thread ID gives no
-indication of which interpreter instance is actually running
-(and hence which corresponding set of values/data-structures
-is actually being referenced); for such situations
-the interpreter ID is more likely to be what you actually want.
+indication of which interpreter instance is actually running and
+hence which corresponding set of values/data-structures is actually
+being referenced.  For such situations the interpreter ID is more
+likely to be what you actually want.
 
 =head2 EXPORT
 
@@ -61,10 +61,10 @@ None by default.  The following function is available:
 
 =head3 interpreter_id()
 
-Returns the current perl interpreter instance's (integer) ID
+Returns an (integer) ID for this instance of the perl interpreter.
 
 Where multiple interpreters have been created to run in
-threads of the current process the IDs returned will be
+threads of the current process, the IDs returned will be
 distinct for each interpreter, regardless of which
 threads are running which interpreters.
 
@@ -89,15 +89,16 @@ on a given host.
 
 =head1 AUTHOR
 
+Packaged by Roger Crew E<lt>crew@cs.stanford.eduE<gt>.
+
 Original XS code is from a posting by ikegami at PerlMonks.
-Packaged by Roger Crew, E<lt>crew@cs.stanford.eduE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
 Copyright (C) 2011 by Roger Crew.
 
 This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.10.1 or,
+it under the same terms as Perl itself, either Perl version 5.8.1 or,
 at your option, any later version of Perl 5 you may have available.
 
 =cut
