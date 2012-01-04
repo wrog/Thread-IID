022218211f59f9305efa49fe9dd4af626d3fb1d3
Roger Crew <crew@cs.stanford.edu>
2012-01-03 21:42:27 -0800

    move setting of ISA/EXPORT_ vars into BEGIN

--- lib/Thread/IID.pm
+++ lib/Thread/IID.pm
@@ -3,28 +3,17 @@ use warnings;
 
 package Thread::IID;
 
- use 5.008001;
+use 5.008001;
 
-require Exporter;
+our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
 
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Thread::IID ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-  interpreter_id
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = ();
-
-our $VERSION;
+BEGIN {
+    require Exporter;
+    @ISA = qw(Exporter);
+    %EXPORT_TAGS = ( 'all' => [ qw(interpreter_id) ] );
+    @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+    @EXPORT = ();
+}
 
 require XSLoader;
 XSLoader::load('Thread::IID', $VERSION);
